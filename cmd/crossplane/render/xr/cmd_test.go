bfdae166f858f6f96d9763b75fc463a4d68bac19
Nikita Z <nkzk95@gmail.com>
2026-06-04 09:51:11 +0200

    test: update newEngineFunc signature
    Signed-off-by: Nikita Z <nkzk95@gmail.com>

--- cmd/crossplane/render/xr/cmd_test.go
+++ cmd/crossplane/render/xr/cmd_test.go
@@ -78,8 +78,8 @@ var includeFunctionResultsOutput string
 //go:embed testdata/cmd/output/include-full-xr.yaml
 var includeFullXROutput string
 
-func newEngineFunc(engine render.Engine) func(*render.EngineFlags, logging.Logger) render.Engine {
-	return func(*render.EngineFlags, logging.Logger) render.Engine {
+func newEngineFunc(engine render.Engine) func(*render.EngineFlags, string, logging.Logger) render.Engine {
+	return func(*render.EngineFlags, string, logging.Logger) render.Engine {
 		return engine
 	}
 }
